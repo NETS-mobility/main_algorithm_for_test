b176dea057752a0d24a288a0eabbf9a7cf5048b8
ssu00 <rachel4535@naver.com>
2022-03-07 13:51:42 +0900

    ✏️ fixed typo

--- src/algorithm/getResult.js
+++ src/algorithm/getResult.js
@@ -2,16 +2,6 @@ const GetResult = (L3, cArr) => {
   if (L3.length == 0) {
     return -1;
   }
-  console.log("L3==", L3);
-  // console.log("carr=", cArr);
-  // L3.forEach((data) => console.log("L3==", data.car_id));
-  // const filteredArr = await cArr.filter(
-  //   (i) => i.car_id == L3.forEach((data) => data.car_id)
-  // );
-
-  // cArr = [{ car_id: 11 }, { car_id: 22 }, { car_id: 33 }, { car_id: 44 }];
-  // L3 = [{ car_id: 11 }, { car_id: 22 }, { car_id: 33 }, { car_id: 44 }];
-  // console.log("f=====", filteredArr);
   const opt = L3.sort(function (a, b) {
     return b.c - a.c;
   });
